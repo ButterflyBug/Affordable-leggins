851f426147d6f384b5ff93935eee9b4f2d6f0d38
Katarzyna Szuper <kszuper1994@gmail.com>
2019-10-20 20:51:22 +0200

    Added function to get rrp for specified product.

--- leggins_list.py
+++ leggins_list.py
@@ -7,25 +7,43 @@ def get_list_of_leggins_from_page(page_number):
     response = requests.get(my_protein_site, {"pageNumber": page_number})
 
     response_html = response.text
-    parsed_response_html = BeautifulSoup(response_html)
+    parsed_response_html = BeautifulSoup(response_html, "html")
 
     leggins = parsed_response_html.find_all(
-        "span", class_="athenaProductBlock_hiddenElement"
+        "div", class_="productListProducts_product"
     )
 
     leggin_name_list = []
 
     for leggin in leggins:
-        leggin_name = leggin["data-product-title"]
-        leggin_id = leggin["data-product-id"]
-        leggin_price = float(leggin["data-product-price"].replace(" zł", ""))
+        leggin_data = leggin.find("span")
+        leggin_name = leggin_data["data-product-title"]
+        leggin_id = leggin_data["data-product-id"]
+        leggin_price = float(
+            leggin_data["data-product-price"].replace(" zł", ""))
+
         leggin_name_list.append({
             "leggin_name": leggin_name,
             "leggin_id": leggin_id,
-            "leggin_price": leggin_price
+            "leggin_price": leggin_price,
         })
 
     return leggin_name_list
+
+
+def get_rrp_from_single_site(identificator):
+    single_leggin_site = "https://www.myprotein.pl/" + \
+        str(identificator) + ".html"
+    single_response = requests.get(single_leggin_site)
+
+    single_response_html = single_response.text
+    parsed_single_response_html = BeautifulSoup(single_response_html, "html")
+
+    leggin_rrp = float(parsed_single_response_html.find(
+        "p", class_="productPrice_rrp"
+    ).text.replace(" zł", "").replace("RRP: ", ""))
+
+    return leggin_rrp
 
 
 def get_list_of_leggins():
@@ -43,5 +61,5 @@ def get_list_of_leggins():
 
 print(get_list_of_leggins())
 
-# mainContent > section:nth-child(2) > div.responsiveProductListPage >
-# main > div.productListProducts > div > div:nth-child(1) > div > span
+for element in get_list_of_leggins():
+    print(element)
