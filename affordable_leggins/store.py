182d7f1acfdb66c96941635e9bd300766a2769bb
Katarzyna Szuper <kszuper1994@gmail.com>
2019-12-04 22:16:21 +0100

    Read data from a file
    Downloaded data stored in a file can be read and turned back into
    Python's structure what makes it possible to work with leggins details.

--- affordable_leggins/store.py
+++ affordable_leggins/store.py
@@ -4,9 +4,25 @@ import datetime
 from affordable_leggins.leggins_list import get_list_of_leggins
 
 
-def store_data():
+def make_directory(directory_name):
+    try:
+        os.mkdir("./" + directory_name)
+    except FileExistsError:
+        pass
+
+
+def store_data(directory_name):
     date_name = datetime.date.today().strftime("%d-%m-%Y")
-    os.mkdir("./leggins_lists")
-    with open("leggins_lists/leggins_lists_" + date_name + ".json", "w") as file:
+    make_directory(directory_name)
+    with open(directory_name + "/leggins_lists_" + date_name + ".json", "w") as file:
         json.dump(get_list_of_leggins(), file)
         return file
+
+
+def read_data(directory_name, day, month, year):
+    file_name = (
+       directory_name + "/leggins_lists_" + day + "-" + month + "-" + year + ".json"
+    )
+    with open(file_name, "r") as file:
+        data_to_read = file.read()
+        return json.loads(data_to_read)
