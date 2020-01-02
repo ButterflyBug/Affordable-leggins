83e544a397bf8c016f118506204342f66e842501
kate <kszuper1994@gmail.com>
2020-01-02 22:30:49 +0100

    Provide with list of leggins filtered by name

--- affordable_leggins/filters.py
+++ affordable_leggins/filters.py
@@ -2,3 +2,12 @@ def filter_size(leggins, size):
     result = list(filter(lambda leggin: size in leggin["sizes"], leggins))
     if result:
         return result
+
+
+def filter_name(leggins, name, reject=False):
+    if reject:
+        result = list(filter(lambda leggin: name not in leggin["leggin_name"], leggins))
+    else:
+        result = list(filter(lambda leggin: name in leggin["leggin_name"], leggins))
+
+    return result
