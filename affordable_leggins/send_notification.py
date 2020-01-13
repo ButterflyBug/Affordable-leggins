bdd656b1a71f36930ef52085eaedb8ad968dfb72
kate <kszuper1994@gmail.com>
2020-01-13 20:53:21 +0100

    Send notification using django template
    Upcoming notifications will be received thanks to possibilities provided by Django
    and its engine which enables running a customize template for the message.

--- affordable_leggins/send_notification.py
+++ affordable_leggins/send_notification.py
@@ -2,16 +2,18 @@ import os
 import textwrap
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+from django.template import Context, Template, Engine
 
 
 def send_email(email, subject, message_body):
     message = Mail(
-        from_email='notifications@leggins.com',
+        from_email="notifications@leggins.com",
         to_emails=email,
         subject=subject,
-        plain_text_content=message_body)
+        plain_text_content=message_body,
+    )
     try:
-        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
         response = sg.send(message)
         print(response.status_code)
         print(response.body)
@@ -30,28 +32,20 @@ def subject():
 
 def single_leggin_format(product):
     leggin_url = f"https://www.myprotein.pl/{product['leggin_id']}.html"
-    return f"* {product['leggin_name']} ({product['leggin_price']}.00 PLN) - {leggin_url}"
+    return (
+        f"* {product['leggin_name']} ({product['leggin_price']}.00 PLN) - {leggin_url}"
+    )
+
+
+# template = Template("My name is {{ my_name }}.")
+
+# >>> context = Context({"my_name": "Adrian"})
+# >>> template.render(context)
 
 
 def message(mail_address, products):
-    formatted_leggins = "\n            ".join(list(map(single_leggin_format, products)))
-    if products:
-        expected_message = f"""
-            Hello {mail_address},
+    message = open("affordable_leggins/templates/notification_email.txt", "r").read()
+    context = Context({"email_address": mail_address, "leggins": products})
+    engine = Engine()
 
-            We have found products which meet your criteria:
-
-            {formatted_leggins}
-
-            Happy hunting!
-        """
-    else:
-        expected_message = f"""
-            Hello {mail_address},
-
-            We have not found products which meet your criteria.
-
-            Happy hunting!
-        """
-
-    return textwrap.dedent(expected_message).strip()
+    return engine.from_string(message).render(context)
