a73c5d884eb16de3b5b462d235ee0e24dbf1e9a2
kate <kszuper1994@gmail.com>
2020-02-09 19:23:07 +0100

    Simplify and speed up tests thanks to mocking
    The test's flow is improved by mock package and patch fixture.

--- tests/test_store_database.py
+++ tests/test_store_database.py
@@ -1,27 +1,39 @@
 import pytest
 import datetime
-from affordable_leggins.store import store_data, read_data
+import mock
+from affordable_leggins.store import (
+    store_data,
+    read_data,
+    store_data_in_database,
+    get_list_of_leggins,
+)
 from affordable_leggins.storage.models import Leggin
-from affordable_leggins.store import store_data_in_database
+from fixtures import leggin_with_size_xs
 
 
+@mock.patch("affordable_leggins.store.get_list_of_leggins")
 @pytest.mark.django_db
-@pytest.mark.vcr("test_store_at_least_one_leggin_in_database")
-def test_store_at_least_one_leggin_in_database():
+def test_store_at_least_one_leggin_in_database(
+    mocked_get_list_of_leggins, leggin_with_size_xs
+):
+    mocked_get_list_of_leggins.return_value = [leggin_with_size_xs]
     store_data_in_database()
     assert Leggin.objects.all().count() != 0
 
 
+@mock.patch("affordable_leggins.store.get_list_of_leggins")
 @pytest.mark.django_db
-@pytest.mark.vcr("test_store_at_least_one_leggin_in_database")
-def test_store_specific_leggin_in_database():
+def test_store_specific_leggin_in_database(
+    mocked_get_list_of_leggins, leggin_with_size_xs
+):
+    mocked_get_list_of_leggins.return_value = [leggin_with_size_xs]
     store_data_in_database()
     assert (
         Leggin.objects.filter(
-            name="Legginsy Curve - Czarne",
-            price=159.0,
+            name="Bezszwowe",
+            price=40.0,
             rrp=159.0,
-            external_id="12068032",
+            external_id="11871452",
             date=datetime.datetime.now(),
         ).count() != 0
     )
